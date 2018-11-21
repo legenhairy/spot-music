9f1dfc1149d103e111988389d038ef1db493bd3a
Harry Zhang <harryzhang100@gmail.com>
2018-11-20 17:57:20 -0800

    minor style changes

--- src/Profile.js
+++ src/Profile.js
@@ -3,16 +3,15 @@ import './App.css';
 
 class Profile extends Component{
   render() {
-  	console.log('this.props', this.props);	
   	let artist = {name:'', followers: {total: ''}};
   	if (this.props.artist !== null) {
   	  artist = this.props.artist;	
   	}
 
   	return (
-  	  <div>
-  	  	<div>{artist.name}</div>	
-  	  	<div>{artist.followers.total}</div>
+  	  <div className="profile">
+  	  	<div className="name">{artist.name}</div>	
+  	  	<div className="followers">{artist.followers.total}</div>
   	  </div>
   	)
   }
